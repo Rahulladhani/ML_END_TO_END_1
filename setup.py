b440634012f12594fa109a32d197b360171826d1
Rahul Ladhani <117468044+Rahulladhani@users.noreply.github.com>
2024-06-22 11:43:43 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+
+def get_requirments(file_path:str)->List[str]:
+''' 
+this function will return the list of requirments
+'''
+
+requirments=[]
+with open(file_path)as file_obj:
+    requirments=file_obj.readlines()
+    requirments=[req.replace("\n","")for req in requirments]
+
+    if HYPEN_E_DOT in requirments:
+        requirments.remove( HYPEN_E_DOT)
+
+return requirments
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Rahul',
+author_email='Rahulladhani11@gmail.com',
+packages=find_packages(),
+install_requires=get_requirments('requirments.txt')
+
+
+
+)
